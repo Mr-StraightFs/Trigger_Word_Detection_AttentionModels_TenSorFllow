ba1cee9e857a7ec24ae34c96c7c10aee15e85503
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-09-17 16:51:44 +0100

    Define and Implement the Model usng the TF Functional APIs

--- Trigger_Word_Detection.py
+++ Trigger_Word_Detection.py
@@ -248,5 +248,53 @@ Y = np.array(Y)
 X_dev = np.load("./XY_dev/X_dev.npy")
 Y_dev = np.load("./XY_dev/Y_dev.npy")
 
+# Define and implement the Model
+def modelf(input_shape):
+    """
+    Function creating the model's graph in Keras.
+
+    Argument:
+    input_shape -- shape of the model's input data (using Keras conventions)
+
+    Returns:
+    model -- Keras model instance
+    """
+
+    X_input = Input(shape=input_shape)
+    # Step 1: CONV layer
+    # Add a Conv1D with 196 units, kernel size of 15 and stride of 4
+    X = Conv1D(196, kernel_size=15, strides=4)(X_input)
+    # Batch normalization
+    X = BatchNormalization()(X)
+    # ReLu activation
+    X = Activation('relu')(X)
+    # dropout (use 0.8)
+    X = Dropout(0.8)(X)
+
+    # Step 2: First GRU Layer
+    # GRU (use 128 units and return the sequences)
+    X = GRU(units=128, return_sequences=True)(X)
+    # dropout (use 0.8)
+    X = Dropout(0.8)(X)
+    # Batch normalization.
+    X = BatchNormalization()(X)
+
+    # Step 3: Second GRU Layer
+    # GRU (use 128 units and return the sequences)
+    X = GRU(units=128, return_sequences=True)(X)
+    # dropout (use 0.8)
+    X = Dropout(0.8)(X)
+    # Batch normalization
+    X = BatchNormalization()(X)
+    # dropout (use 0.8)
+    X = Dropout(0.8)(X)
+
+    # Step 4: Time-distributed dense layer (≈1 line)
+    # TimeDistributed  with sigmoid activation
+    X = TimeDistributed(Dense(1, activation="sigmoid"))(X)
+
+    model = Model(inputs=X_input, outputs=X)
+
+    return model
 
 
