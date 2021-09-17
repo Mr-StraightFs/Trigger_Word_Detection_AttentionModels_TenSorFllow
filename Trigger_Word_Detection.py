210281ac39c53211eceb5fe40f2a14bbe1f41f3a
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-09-17 16:32:08 +0100

    Implement and Test Helper Functions

--- Trigger_Word_Detection.py
+++ Trigger_Word_Detection.py
@@ -27,4 +27,113 @@ print("background len should be 10,000, since it is a 10 sec clip\n" + str(len(b
 print("activate[0] len may be around 1000, since an `activate` audio clip is usually around 1 second (but varies a lot) \n" + str(len(activates[0])),"\n")
 print("activate[1] len: different `activate` clips can have different lengths\n" + str(len(activates[1])),"\n")
 
+# Implement Helper Functions
+# get_random_time_segment(segment_ms) : Retrieves a random time segment from the background audio.
+# is_overlapping(segment_time, existing_segments) : Checks if a time segment overlaps with existing segments
+# insert_audio_clip(background, audio_clip, existing_times) : Inserts an audio segment at a random time in the background audio
+# Uses the functions get_random_time_segment and is_overlapping
+# insert_ones(y, segment_end_ms) : Inserts additional 1's into the label vector y after the word "activate"
+
+def get_random_time_segment(segment_ms):
+    """
+    Gets a random time segment of duration segment_ms in a 10,000 ms audio clip.
+
+    Arguments:
+    segment_ms -- the duration of the audio clip in ms ("ms" stands for "milliseconds")
+
+    Returns:
+    segment_time -- a tuple of (segment_start, segment_end) in ms
+    """
+
+    segment_start = np.random.randint(low=0,
+                                      high=10000 - segment_ms)  # Make sure segment doesn't run past the 10sec background
+    segment_end = segment_start + segment_ms - 1
+
+    return (segment_start, segment_end)
+
+
+def is_overlapping(segment_time, previous_segments):
+    """
+    Checks if the time of a segment overlaps with the times of existing segments.
+
+    Arguments:
+    segment_time -- a tuple of (segment_start, segment_end) for the new segment
+    previous_segments -- a list of tuples of (segment_start, segment_end) for the existing segments
+
+    Returns:
+    True if the time segment overlaps with any of the existing segments, False otherwise
+    """
+
+    segment_start, segment_end = segment_time
+
+    # Step 1: Initialize overlap as a "False" flag
+    overlap = False
+
+    # Step 2: loop over the previous_segments start and end times.
+    # Compare start/end times and set the flag to True if there is an overlap
+    for previous_start, previous_end in previous_segments:  # @KEEP
+        if segment_start <= previous_end and previous_start <= segment_end:
+            overlap = True
+            break
+
+    return overlap
+
+# Test the is_overlapping Function
+overlap1 = is_overlapping((950, 1430), [(2000, 2550), (260, 949)])
+overlap2 = is_overlapping((2305, 2950), [(824, 1532), (1900, 2305), (3424, 3656)])
+print("Overlap 1 = ", overlap1)
+print("Overlap 2 = ", overlap2)
+
+
+def insert_audio_clip(background, audio_clip, previous_segments):
+    """
+    Insert a new audio segment over the background noise at a random time step, ensuring that the
+    audio segment does not overlap with existing segments.
+
+    Arguments:
+    background -- a 10 second background audio recording.
+    audio_clip -- the audio clip to be inserted/overlaid.
+    previous_segments -- times where audio segments have already been placed
+
+    Returns:
+    new_background -- the updated background audio
+    """
+
+    # Get the duration of the audio clip in ms
+    segment_ms = len(audio_clip)
+
+    # Step 1: Use one of the helper functions to pick a random time segment onto which to insert
+    # the new audio clip
+    segment_time = get_random_time_segment(segment_ms)
+
+    # Step 2: Check if the new segment_time overlaps with one of the previous_segments. If so, keep
+    # picking new segment_time at random until it doesn't overlap. To avoid an endless loop
+    # we retry 5 times
+    retry = 5
+    while is_overlapping(segment_time, previous_segments) and retry >= 0:
+        segment_time = get_random_time_segment(segment_ms)
+        retry = retry - 1
+
+        # print(segment_time)
+    # if last try is not overlaping, insert it to the background
+    if not is_overlapping(segment_time, previous_segments):
+        # Step 3: Append the new segment_time to the list of previous_segmen
+        previous_segments.append(segment_time)
+        # Step 4: Superpose audio segment and background
+        new_background = background.overlay(audio_clip, position=segment_time[0])
+    else:
+        # print("Timeouted")
+        new_background = background
+        segment_time = (10000, 10000)
+
+    return new_background, segment_time
+
+# Test the Function Overlapping
+np.random.seed(5)
+audio_clip, segment_time = insert_audio_clip(backgrounds[0], activates[0], [(3790, 4400)])
+audio_clip.export("insert_test.wav", format="wav")
+print("Segment Time: ", segment_time)
+IPython.display.Audio("insert_test.wav")
+
+
 
