9961b068a7d30c00d42abef05d2e8d588403a7a2
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-09-17 16:59:06 +0100

    Insert a chime to acknowledge the activate trigger

--- Trigger_Word_Detection.py
+++ Trigger_Word_Detection.py
@@ -347,3 +347,29 @@ def detect_triggerword(filename):
     plt.ylabel('probability')
     plt.show()
     return predictions
+
+# Insert a chime to acknowledge the "activate" trigger
+chime_file = "audio_examples/chime.wav"
+
+
+def chime_on_activate(filename, predictions, threshold):
+    audio_clip = AudioSegment.from_wav(filename)
+    chime = AudioSegment.from_wav(chime_file)
+    Ty = predictions.shape[1]
+    # Step 1: Initialize the number of consecutive output steps to 0
+    consecutive_timesteps = 0
+    # Step 2: Loop over the output steps in the y
+    for i in range(Ty):
+        # Step 3: Increment consecutive output steps
+        consecutive_timesteps += 1
+        # Step 4: If prediction is higher than the threshold and more than 20 consecutive output steps have passed
+        if consecutive_timesteps > 20:
+            # Step 5: Superpose audio and background using pydub
+            audio_clip = audio_clip.overlay(chime, position=((i / Ty) * audio_clip.duration_seconds) * 1000)
+            # Step 6: Reset consecutive output steps to 0
+            consecutive_timesteps = 0
+        # if amplitude is smaller than the threshold reset the consecutive_timesteps counter
+        if predictions[0, i, 0] < threshold:
+            consecutive_timesteps = 0
+
+    audio_clip.export("chime_output.wav", format='wav')
