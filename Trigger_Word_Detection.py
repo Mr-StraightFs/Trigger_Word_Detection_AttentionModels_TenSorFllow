ddee734f53abad8aa1481151696e43fe55c0d13e
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-09-17 16:36:38 +0100

    Insert-ones function

--- Trigger_Word_Detection.py
+++ Trigger_Word_Detection.py
@@ -136,4 +136,35 @@ print("Segment Time: ", segment_time)
 IPython.display.Audio("insert_test.wav")
 
 
+def insert_ones(y, segment_end_ms):
+    """
+    Update the label vector y. The labels of the 50 output steps strictly after the end of the segment
+    should be set to 1. By strictly we mean that the label of segment_end_y should be 0 while, the
+    50 following labels should be ones.
+
+
+    Arguments:
+    y -- numpy array of shape (1, Ty), the labels of the training example
+    segment_end_ms -- the end time of the segment in ms
+
+    Returns:
+    y -- updated labels
+    """
+    _, Ty = y.shape
+
+    # duration of the background (in terms of spectrogram time-steps)
+    segment_end_y = int(segment_end_ms * Ty / 10000.0)
+
+    if segment_end_y < Ty:
+        # Add 1 to the correct index in the background label (y)
+
+        for i in range(segment_end_y + 1, segment_end_y + 51):
+            if i < Ty:
+                y[0, i] = 1
+
+    return y
+
+
+
+
 
